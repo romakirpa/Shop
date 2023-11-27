95f8c0743af2b392b88cadd07c15999d794041a5
romakirpa <120048330+romakirpa@users.noreply.github.com>
2023-11-27 11:16:00 +0200

    Added empty product controller, service, router.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const listeningPort = process.env.PORT || 3000;
 
 app.get("/", (req, res) => { 
     res.send("base page"); 
-}); 
+});
 
 app.listen(listeningPort, () => { 
     console.log(`Server started. API is listening on port ${listeningPort}`); 
